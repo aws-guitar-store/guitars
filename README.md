252dde33bf663d11f69a3697483fde48a65804ac
Jeromy Van Dusen <jeromy.vandusen@gmail.com>
2020-10-28 11:21:05 -0500

    Create initial project and implement list of guitars endpoint

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spring Boot Micro-Services
+
+## Guitar Store - Guitars micro-service
+
+### Startup
+
+`mvn spring:boot:run`
+
+### Shutdown
+
+`CTRL-C`
---
+++ src/main/java/com/example/sbms/guitars/GuitarsApplication.java
@@ -0,0 +1,11 @@
+package com.example.sbms.guitars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarsApplication.class, args);
+	}
+}
